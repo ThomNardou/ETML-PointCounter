e0fee7192a3befe6f724e526977aada3a4659297
ReactedTitan880 <thomasnardou@icloud.com>
2024-08-29 21:18:28 +0200

    add module selection (not finished)

--- etml-pointcointer/src/app/components/NavBar.tsx
+++ etml-pointcointer/src/app/components/NavBar.tsx
@@ -4,7 +4,7 @@ export default function NavBarComponent() {
     return (
         <>
             <header className="w-full h-20 flex justify-center mt-10 mb-10">
-                <div className="w-6/12 h-20 rounded-full fixed bg-slate-800 flex items-center">
+                <div className="w-6/12 h-20 rounded-full  bg-slate-800 flex items-center">
                     <nav className="w-full flex justify-end">
                         <ul className="navBar flex w-2/5 mr-12 justify-between items-center text-white">
                             <li>Gamification</li>
--- etml-pointcointer/src/app/page.tsx
+++ etml-pointcointer/src/app/page.tsx
@@ -1,45 +1,132 @@
 import Image from "next/image";
 import Table from "@mui/material/Table";
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import * as React from "react";
 
 const rows = [
-  { studentId: 1, name: 'pk88yte', points: 100 },
-  { studentId: 2, name: 'pk88yte', points: 150 },
-  { studentId: 3, name: 'pk88yte', points: 200 },
-  { studentId: 4, name: 'pk88yte', points: 250 },
-  { studentId: 5, name: 'pk88yte', points: 300 },
+  { studentId: 1, name: "pk88yte", points: 100 },
+  { studentId: 2, name: "pk88yte", points: 150 },
+  { studentId: 3, name: "pk88yte", points: 200 },
+  { studentId: 4, name: "pk88yte", points: 250 },
+  { studentId: 5, name: "pk88yte", points: 300 },
 ];
 
+const modules = [
+  { id: 1, name: "I347" },
+  { id: 2, name: "I320" },
+  { id: 3, name: "I164" },
+  { id: 4, name: "I254" },
+  { id: 5, name: "I236" },
+];
 
 export default function Home() {
+
+  const [module, setModule] = React.useState<number>(0);
+
   return (
     <main>
-      <TableContainer component={Paper} sx={{backgroundColor: "#23304d", width: "50%"}}>
-        <Table>
-          
-          <TableHead sx={{borderBottomColor: "darkgray", borderBottomWidth: "5px"}}>
-            <TableRow sx={{bgcolor: "#475b87"}}>
-              <TableCell>Nom</TableCell>
-              <TableCell sx={{borderLeftColor: "darkgray", borderLeftWidth: "2px"}}>Points</TableCell>
-            </TableRow>
-          </TableHead>
+      <div className="highScore pt-28 w-full text-center">
+        <Box sx={{ maxWidth: 150 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Module</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={0}
+              label="Module"
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
 
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.studentId}>
-                <TableCell sx={{borderBottomColor: "darkgray"}}>{row.name}</TableCell>
-                <TableCell sx={{borderBottomColor: "darkgray", borderLeftColor: "darkgray", borderLeftWidth: "2px"}}>{row.points}</TableCell>
+        <TableContainer
+          component={Paper}
+          sx={{
+            backgroundColor: "#23304d",
+            width: "50%",
+            display: "block",
+            margin: "64px auto",
+          }}
+        >
+          <Table>
+            <TableHead
+              sx={{ borderBottomColor: "darkgray", borderBottomWidth: "5px" }}
+            >
+              <TableRow sx={{ bgcolor: "#475b87" }}>
+                <TableCell
+                  sx={{ fontSize: "18px", color: "white", width: "20%" }}
+                >
+                  Place
+                </TableCell>
+
+                <TableCell
+                  sx={{
+                    borderLeftColor: "darkgray",
+                    borderLeftWidth: "2px",
+                    fontSize: "18px",
+                    color: "white",
+                    width: "40%",
+                  }}
+                >
+                  Nom
+                </TableCell>
+                <TableCell
+                  sx={{
+                    borderLeftColor: "darkgray",
+                    borderLeftWidth: "2px",
+                    fontSize: "18px",
+                    color: "white",
+                    width: "40%",
+                  }}
+                >
+                  Points Totaux
+                </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
+            </TableHead>
 
-        </Table>
-      </TableContainer>
+            <TableBody>
+              {rows.map((row, index) => (
+                <TableRow key={row.studentId}>
+                  <TableCell sx={{ color: "white" }}>{index + 1}</TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottomColor: "darkgray",
+                      borderLeftColor: "darkgray",
+                      borderLeftWidth: "2px",
+                      color: "white",
+                    }}
+                  >
+                    {row.name}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      borderBottomColor: "darkgray",
+                      borderLeftColor: "darkgray",
+                      borderLeftWidth: "2px",
+                      color: "white",
+                    }}
+                  >
+                    {row.points}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </main>
   );
 }
