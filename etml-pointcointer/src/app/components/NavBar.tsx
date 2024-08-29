440d220a8a364aca320c60fcc2cccc348a672273
ReactedTitan880 <thomasnardou@icloud.com>
2024-08-29 20:29:34 +0200

    add point table

--- etml-pointcointer/src/app/components/NavBar.tsx
+++ etml-pointcointer/src/app/components/NavBar.tsx
@@ -4,9 +4,8 @@ export default function NavBarComponent() {
     return (
         <>
             <header className="w-full h-20 flex justify-center mt-10 mb-10">
-                <div className="w-6/12 h-20 rounded-full fixed bg-slate-800">
-                    <nav className="flex justify-between items-center">
-                        <Image src="/logo.svg" alt="PointCointer" width={80} height={80} className="bg-slate-500 ml-12" />
+                <div className="w-6/12 h-20 rounded-full fixed bg-slate-800 flex items-center">
+                    <nav className="w-full flex justify-end">
                         <ul className="navBar flex w-2/5 mr-12 justify-between items-center text-white">
                             <li>Gamification</li>
                             <li>about</li>
