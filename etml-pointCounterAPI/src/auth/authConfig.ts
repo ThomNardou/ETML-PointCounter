353231d2ea26e9bcd71d904bc11458370e8646e2
ReactedTitan880 <thomasnardou@icloud.com>
2024-09-09 11:46:12 +0200

    [Backend] create: create authconfig file

--- etml-pointCounterAPI/src/auth/authConfig.ts
+++ etml-pointCounterAPI/src/auth/authConfig.ts
@@ -1,5 +1,12 @@
 import dotenv from "dotenv";
+import { AppConfig } from "./authProvider";
 
-const msalConfig = {
+dotenv.config()
 
-}
+const appConfig: AppConfig = {
+    instance: process.env.INSTANCE || "ENTER_CLOUD_INSTANCE_HERE",
+    tenantId: process.env.TENANT_ID || "ENTER_TENANT_ID_HERE",
+    clientId: process.env.CLIENT_ID || "ENTER_CLIENT_ID_HERE",
+    clientSecret: process.env.CLIENT_SECRET || "ENTER_CLIENT_SECRET_HERE",
+    redirectUri: process.env.REDIRECT_URI || "ENTER_REDIRECT_URI_HERE",
+};
