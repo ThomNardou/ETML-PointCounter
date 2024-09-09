9285ae340ab7a5b3c85c61279c5562f8d1089316
ReactedTitan880 <thomasnardou@icloud.com>
2024-09-09 12:44:14 +0200

    [Backend] Add: Add the authentification with ms account

--- etml-pointCounterAPI/src/app.ts
+++ etml-pointCounterAPI/src/app.ts
@@ -1,17 +1,91 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { initDb, sequelize } from "./db/sequelize";
+import { login } from "./auth/authService";
+import { PublicClientApplication, ConfidentialClientApplication, AccountInfo } from "@azure/msal-node";
+import { msalConfig } from "./auth/authConfig";
+import session = require("express-session");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.API_PORT || 3000;
 
+const msalClient = new ConfidentialClientApplication(msalConfig)
+
 
 app.use(express.json());
 
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET as string,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false,
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 3,
+        }
+    })
+)
+
+declare module 'express-session' {
+    export interface SessionData {
+        user?: AccountInfo;
+    }
+}
+
+app.get("/login", (req: Request, res: Response) => {
+    const authCodeUrlParameters = {
+        scopes: ["openid", "profile", "User.Read"],
+        redirectUri: "http://localhost:3000/auth/redirect",
+    };
+
+    msalClient
+        .getAuthCodeUrl(authCodeUrlParameters)
+        .then((response) => {
+            res.redirect(response);
+        })
+        .catch((error) => console.log(JSON.stringify(error)));
+});
+
+app.get("/auth/redirect", (req: Request, res: Response) => {
+    const tokenRequest = {
+        code: req.query.code as string,
+        scopes: ["openid", "profile", "User.Read"],
+        redirectUri: "http://localhost:3000/auth/redirect",
+    };
+
+    msalClient
+        .acquireTokenByCode(tokenRequest)
+        .then((response) => {
+            req.session.user = response.account ?? undefined;
+            console.log(response)
+            res.send(`Bonjour, ${response.account?.username}! Vous êtes connecté.`);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Erreur lors de la connexion.");
+        });
+});
+
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la déconnexion.");
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to the PointCounter API");
+    if (req.session.user) {
+        res.send(`Connecté en tant que ${req.session.user.username}`);
+    } else {
+        res.send("Non connecté. <a href='/login'>Se connecter avec Microsoft</a>");
+    }
 });
 
 sequelize.authenticate()
--- etml-pointCounterAPI/src/auth/authConfig.ts
+++ etml-pointCounterAPI/src/auth/authConfig.ts
@@ -1,12 +1,19 @@
-import dotenv from "dotenv";
-import { AppConfig } from "./authProvider";
+import { Configuration } from "@azure/msal-node";
 
-dotenv.config()
-
-const appConfig: AppConfig = {
-    instance: process.env.INSTANCE || "ENTER_CLOUD_INSTANCE_HERE",
-    tenantId: process.env.TENANT_ID || "ENTER_TENANT_ID_HERE",
-    clientId: process.env.CLIENT_ID || "ENTER_CLIENT_ID_HERE",
-    clientSecret: process.env.CLIENT_SECRET || "ENTER_CLIENT_SECRET_HERE",
-    redirectUri: process.env.REDIRECT_URI || "ENTER_REDIRECT_URI_HERE",
+const msalConfig: Configuration = {
+  auth: {
+    clientId: process.env.CLIENT_ID as string,  
+    authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}`, 
+    clientSecret: process.env.CLIENT_SECRET,  
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(loglevel, message, containsPii) {
+        console.log(message);
+      },
+      piiLoggingEnabled: false,
+    },
+  },
 };
+
+export {msalConfig};
