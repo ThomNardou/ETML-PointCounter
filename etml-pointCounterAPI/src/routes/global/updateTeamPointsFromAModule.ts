66186a6e71e5f2165117e8f4bc82c911f40c5927
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-09-18 19:03:01 +0200

    [BACKEND] fix: fix update because it wasn't working

--- etml-pointCounterAPI/src/routes/global/updateTeamPointsFromAModule.ts
+++ etml-pointCounterAPI/src/routes/global/updateTeamPointsFromAModule.ts
@@ -4,19 +4,15 @@ import { Team } from "../../db/sequelize";
 const updateTeamPointsFromAModule = express.Router();
 
 updateTeamPointsFromAModule.put("/team/:idTeam", async (req: Request, res: Response) => {
-    Team.findOne({
-        where: {
-            id_team: req.params.idTeam
-        }
-      }).then((team) => {
-        if (team != null) {
-            team.dataValues.groNbrOfPoints += req.body.points;
-            res.status(200).json({ entity: team })
-        } else {
-            res.status(404).json({ error: "No team was found" })
-        }
-
-    }).catch((error) => {
+    Team.update(req.body, { where: {id_team: req.params.idTeam}})
+    .then((_) =>  {
+        return Team.findByPk(req.params.idTeam).then((updatedTeam) => {
+            if(updatedTeam != null){
+                res.status(200).json({entity: updatedTeam})
+            }
+        })
+    })
+    .catch((error) => {
         res.status(500).json(`An error occured: ${error}`)
     })
 
