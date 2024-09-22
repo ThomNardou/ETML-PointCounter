76b87173714718303c9a7f91a4c765509fde0e32
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-09-22 20:36:17 +0200

    [FRONTEND] feat: allows the import from the .env
    FINALLYYYY!!!!!! 2+ HOURS OF SEARCH

--- etml-pointCounter/next.config.mjs
+++ etml-pointCounter/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    env: {
+      baseUrl: process.env.BASE_URL, // pulls from .env file
+    }
+};
+  
 export default nextConfig;
